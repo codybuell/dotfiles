a8bb153b73b26c782f48e352233c3a7205e4d2f1
codybuell <cody@codybuell.com>
2016-09-03 15:13:49 -0400

    Repair hangups caused by non interactive sessions

--- dotfiles/homestead/after.sh
+++ dotfiles/homestead/after.sh
@@ -4,8 +4,15 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 
-sudo apt-get update
-sudo apt-get -y install php-ldap ntp
-sudo service ntp stop
-sudo ntpdate 0.ubuntu.pool.ntp.org
-sudo service ntp start
+echo "\
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y install php-ldap ntp ntpdate
+service ntp stop
+ntpdate 0.ubuntu.pool.ntp.org
+service ntp start" \
+> /tmp/provision.sh
+
+chmod 755 /tmp/provision.sh
+sudo /tmp/provision.sh
