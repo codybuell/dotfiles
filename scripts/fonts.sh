5c934bfdbef1d15fa296eec92d52bb6924e94092
codybuell <cody@codybuell.com>
2016-10-20 11:51:30 -0400

    Add ttf flavors for windows systems

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,6 +1,7 @@
 # script to install system fonts
 #
-#   if osx
+#   if osx install otf
 #     find fonts/ -type f -exec cp {} /Library/Fonts/ \;
 #
+#   if windows install ttf
 #   adobe source code pro
