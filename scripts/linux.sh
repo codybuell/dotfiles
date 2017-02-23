4de0a487e4e6eb6e24f4f2eb45e0cb54441693ca
codybuell <cody@codybuell.com>
2017-02-23 15:31:16 -0500

    Add dconf configs, rough out linux config script

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -14,7 +14,16 @@ apt-get install \
   ruby \
   ruby-dev \
   vim-nox \
+
 # uninstall apt tmux, curl the latest, ./config && make; make install
 
 # set colemak and caps lock to ctrl
 # /etc/defaults/keyboard -> XKBOPTIONS="ctrl:nocaps" XKBVARIANT="colemak"
+
+# mv .otf fonts to ~/.local/share/fonts
+mkdir -p $HOME/.local/share/fonts
+find fonts -type f -name \*.otf -exec cp {} $HOME/.local/share/fonts/ ;\
+
+# 16.04+ suck in dconf settings
+dconf reset -f /org/gnome/terminal/legacy/profiles:/
+dconf load /org/gnome/terminal/legacy/profiles:/ < dconf/terminal.dconf
