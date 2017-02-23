c86e5da6a8aad9e1b1bb6ec1eea2bf9ec1daad19
codybuell <cody@codybuell.com>
2017-02-23 14:42:20 -0500

    Add some linux configs

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,1 +1,20 @@
 # script to configure linux environment, comprable to brew.sh
+
+# detect system w/uname or lsb_release -a
+
+# if ubuntu
+apt-get install \
+  cmake \
+  exuberant-ctags \
+  gcc \
+  git \
+  libevent-dev \
+  libncurses5-dev \
+  nfs-common
+  ruby \
+  ruby-dev \
+  vim-nox \
+# uninstall apt tmux, curl the latest, ./config && make; make install
+
+# set colemak and caps lock to ctrl
+# /etc/defaults/keyboard -> XKBOPTIONS="ctrl:nocaps" XKBVARIANT="colemak"
