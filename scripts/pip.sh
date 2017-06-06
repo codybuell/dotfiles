32290afe332f2432619ea51068e7842ce0067ad1
codybuell <cody@codybuell.com>
2017-06-06 11:21:05 -0400

    Add dependency for weechat scripts

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -1,3 +1,4 @@
 # script to install pip packages
 #
 #   jmespath-terminal           json manipulation and parsing tool (run as `jpterm`)
+#   pync                        required for notification-center.py weechat plugin
