25cf16a133ae25dc18832bbcf523fffb37a278c6
codybuell <cody@codybuell.com>
2017-06-13 12:44:26 -0400

    Add pip dependency

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -2,3 +2,4 @@
 #
 #   jmespath-terminal           json manipulation and parsing tool (run as `jpterm`)
 #   pync                        required for notification-center.py weechat plugin
+#   websocket-client            required for wee-slack weechat plugin
