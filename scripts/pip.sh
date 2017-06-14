1ee3a970b435657c1dd39aeddf5db3c65c4cef36
codybuell <cody@codybuell.com>
2017-06-14 13:27:51 -0400

    Add dependency for cookiemonster

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -3,3 +3,4 @@
 #   jmespath-terminal           json manipulation and parsing tool (run as `jpterm`)
 #   pync                        required for notification-center.py weechat plugin
 #   websocket-client            required for wee-slack weechat plugin
+#   browsercookie               for getting browser cookies with cookiemonster via cli
