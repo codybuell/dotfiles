d6e56bfb0568367f1a6607e0546cf67837e59e0e
codybuell <cody@codybuell.com>
2017-08-07 08:17:11 -0400

    Fix perms and remove spaces

--- dotfiles/mutt/hooks/postsync/tmux.sh
+++ dotfiles/mutt/hooks/postsync/tmux.sh
@@ -6,8 +6,8 @@ INBOXHOME="$HOME/.mail/Home/Home/new/"
 INBOXWORK="$HOME/.mail/Work/Work/new/"
 INBOXCLIENT="$HOME/.mail/Client/Client/new/"
 
-NEWHOME=`find $INBOXHOME -type f | wc -l`
-NEWWORK=`find $INBOXWORK -type f | wc -l`
-NEWCLIENT=`find $INBOXCLIENT -type f | wc -l`
+NEWHOME=`find $INBOXHOME -type f | wc -l | sed 's/ //g'`
+NEWWORK=`find $INBOXWORK -type f | wc -l | sed 's/ //g'`
+NEWCLIENT=`find $INBOXCLIENT -type f | wc -l | sed 's/ //g'`
 
 tmux rename-window -t home:1 "H:$NEWHOME W:$NEWWORK C:$NEWCLIENT"
