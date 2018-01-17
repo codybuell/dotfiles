763b98e246984a518ed3de92ab437dcd8dc0cb0b
codybuell <cody@codybuell.com>
2018-01-17 10:29:51 -0500

    Add bundler gem for markdown emails

--- scripts/gem.sh
+++ scripts/gem.sh
@@ -1,2 +1,7 @@
 # gem install slackcat         # cat files and streams to slack
 #             ghi              # github issues command line
+#             bundler          # needed for mutt sending markdown emails!!
+
+
+# bundle install               # installs mime types needed for bundler
+
