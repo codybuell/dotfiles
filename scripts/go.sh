d3c6bc3218f02088e0884fc213ce05ad0ea6bca1
codybuell <cody@codybuell.com>
2018-03-08 11:24:19 -0500

    Add passage config

--- scripts/go.sh
+++ scripts/go.sh
@@ -19,3 +19,11 @@
 go get github.com/wincent/passage
 go get github.com/keybase/go-keychain
 go get github.com/Netflix-Skunkworks/go-jira
+
+# build and configure passage to start on boot
+cd $GOPATH/src/wincent/passage
+go build
+sudo cp passage /usr/local/bin
+cp contrib/com.wincent.passage.plist ~/Library/LaunchAgents
+# NOTE: This won't work if run inside a tmux session:
+launchctl load -w -S Aqua ~/Library/LaunchAgents/com.wincent.passage.plist
