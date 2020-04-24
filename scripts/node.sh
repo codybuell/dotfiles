1616ad1c12de73e868d5484628b571cf2555a655
codybuell <cody@codybuell.com>
2020-04-23 23:29:03 -0400

    Adding bash and js-ts langservers

--- scripts/node.sh
+++ scripts/node.sh
@@ -21,17 +21,19 @@
 ###################
 
 PACKAGES=( \
-  'asciicast2gif' \               # convert asciinema json files to gifs
-  'bower' \                       # web development 3rd party dependency / asset manager
-  'gistup' \                      # a tool for creating gists from the terminal
-  'gulp' \                        # asset compiler for web development
-  'http-server' \                 # a light-weight web server for development and testing
-  'neovim' \                      # node provider for neovim
-  'topojson' \                    # tool for converting shape files into json for d3
-  'jsonlint' \                    # json syntax checking utility
-  'svgo' \                        # svg optimization utility
-  'wscat' \                       # websocket command line client
-  'yarn' \                        # package manager with version locking and caching
+  'asciicast2gif' \                         # convert asciinema json files to gifs
+  'bower' \                                 # web development 3rd party dependency / asset manager
+  'gistup' \                                # a tool for creating gists from the terminal
+  'gulp' \                                  # asset compiler for web development
+  'http-server' \                           # a light-weight web server for development and testing
+  'neovim' \                                # node provider for neovim
+  'topojson' \                              # tool for converting shape files into json for d3
+  'jsonlint' \                              # json syntax checking utility
+  'svgo' \                                  # svg optimization utility
+  'wscat' \                                 # websocket command line client
+  'yarn' \                                  # package manager with version locking and caching
+  'bash-language-server' \                  # bash language server
+  'javascript-typescript-langserver' \      # javascript / typescript language server
 )
 
 #######################
