87fa4e9cb441de78d55ed7bfcdce6ad49bddecf1
codybuell <cody@codybuell.com>
2020-09-23 20:34:27 -0400

    Refactor to use library, clean header + comments

--- scripts/repos.sh
+++ scripts/repos.sh
@@ -14,13 +14,7 @@
 #
 # Usage: ./repos.sh
 
-#########################
-#                       #
-#  Establish Variables  #
-#                       #
-#########################
-
-CONFGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &&  cd ../ && pwd )"
+source scripts/library.sh
 
 ######################
 #                    #
@@ -28,42 +22,13 @@ CONFGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &&  cd ../ && pwd )"
 #                    #
 ######################
 
-readconfig() {
-
-  CONFIGVARS=()
-  shopt -s extglob
-  configfile="$CONFGDIR/.config"
-  [[ -e $configfile ]] && {
-    tr -d '\r' < $configfile > $configfile.tmp
-    while IFS='= ' read -r lhs rhs; do
-      if [[ ! $lhs =~ ^\ *# && -n $lhs ]]; then
-        rhs="${rhs%%\#*}"    # del in line right comments
-        rhs="${rhs%%*( )}"   # del trailing spaces
-        rhs="${rhs%\"*}"     # del opening string quotes
-        rhs="${rhs#\"*}"     # del closing string quotes
-        export $lhs="$rhs"
-        CONFIGVARS+="$lhs "
-      fi
-    done < $configfile.tmp
-    export CONFIGVARS
-  } || {
-    printf "\033[0;31mno configuration file detected\033[0m\n"
-    exit 1
-  }
-  rm $configfile.tmp
-
-}
-
-prettyprint() {
-
-  printf "$1" | awk '{file=$1;$1="";printf "  %-40s %s\n", file, $0}' | sed "s/ /,/g;s/\([^,]\),/\1 /g;s/,\([^,]\)/ \1/g;s/^,/ /;s/,/./g";
-
-}
+# Clone Repos
+#
+# Clone repos to the $Repos path as defined by every $REPO.* variable in
+# $CONFGDIR/.config.
 
 clonerepos() {
-
   printf "\033[0;34msetting up repositories...\033[0m\n"
-
   for r in ${CONFIGVARS[@]}; do
     VAR=$r
     eval VAL=\$$r
@@ -71,14 +36,13 @@ clonerepos() {
       MAKEMODEL=`echo $VAL | sed 's/\// /g;s/\.git//;s/:/ /g' | awk '{print $(NF-1)"/"$NF}'`
       TARGETPATH=${Repos}/`echo $VAL | sed 's/\// /g;s/\.git//' | awk 'END {print $NF}'`
       [[ ! -d $TARGETPATH ]] && {
-        prettyprint "  '${MAKEMODEL}' \033[0;32mcloning\033[0m\n"
+        prettyprint "  ${MAKEMODEL}:\033[0;32mcloning\033[0m\n"
         git clone $VAL $TARGETPATH > /dev/null 2>&1
       } || {
-        prettyprint "  '${MAKEMODEL}' \033[0;32malready cloned\033[0m\n"
+        prettyprint "  ${MAKEMODEL}:\033[0;32malready cloned\033[0m\n"
       }
     }
   done
-
 }
 
 ############
