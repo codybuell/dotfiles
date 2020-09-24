cd49972ad09bcc14fb822810643ebacb5ba20559
codybuell <cody@codybuell.com>
2020-09-23 21:07:35 -0400

    Clean up headers, remove unused packages

--- scripts/gem.sh
+++ scripts/gem.sh
@@ -6,13 +6,13 @@
 #
 # Author(s): Cody Buell
 #
-# Revisions: 2018.01.17 Initial framework.
-#
 # Requisite: Ruby
 #
-# Task List: 
+# Tasks: 
 #
-# Usage: ./gem.sh
+# Usage: make gem
+#        ./gem.sh
+#        sh -c "$(curl -fsSL https://raw.githubusercontent.com/codybuell/dotfiles/master/scripts/gem.sh)"
 
 #################
 #               #
@@ -23,7 +23,6 @@
 #gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 #\curl -sSL https://get.rvm.io | bash -s stable --ruby
 
-
 ###################
 #                 #
 #  package lists  #
@@ -32,9 +31,7 @@
 
 PACKAGES=( \
   'bundler' \                     # needed for mutt sending markdown emails
-  'ghi' \                         # github issues command line
   'neovim' \                      # neovim client, required to use ruby with neovim
-  'slackcat' \                    # cat files and streams to slack
 )
 
 #######################
