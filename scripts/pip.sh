509660e4402b571a461d3b54bcd78490baf9138b
codybuell <cody@codybuell.com>
2020-09-23 20:44:48 -0400

    Clean header, remove Python 2, move packages to 3
    Python 2 is no longer supported and has been subsequently removed from
    brew. All needed packages are now available via pip3 installed via brew.

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -6,13 +6,13 @@
 #
 # Author(s): Cody Buell
 #
-# Revisions: 2018.01.17 Initial framework.
-#
 # Requisite: Python2 & Python3
 #
 # Task List: 
 #
-# Usage: ./pip.sh
+# Usage: make pip
+#        ./pip.sh
+#        sh -c "$(curl -fsSL https://raw.githubusercontent.com/codybuell/dotfiles/master/scripts/pip.sh)"
 
 ###################
 #                 #
@@ -20,38 +20,23 @@
 #                 #
 ###################
 
-# standard python 2 packages
-PYTHON2=( \
+# standard Python 3 packages
+PYTHON3=( \
     'browsercookie' \               # for getting browser cookies with cookiemonster via cli
+    'click' \                       # required for mattermost auth cookie gathering script
+    'commandt.score' \              # search scoring utility used in custom deoplete filter
     'feedparser' \                  # rss support for weechat plugin
     'jmespath-terminal' \           # json manipulation and parsing tool (run as `jpterm`)
     'neovim' \                      # neovim client, required to use python with neovim
     'pynvim' \                      # neovim client, required to use python with neovim
+    'selenium' \                    # same as click
     'websocket-client' \            # required for wee-slack weechat plugin
     'python-language-server' \      # a better python language server than pyls??
 )
-#   'pyls' \                        # python languageserver
-
-# osx specific python 2 packages
-OSXPYTHON2=( \
-    'pync' \                        # required for notification-center.py weechat plugin
-)
-
-# standard Python 3 packages
-PYTHON3=( \
-    'click' \                       # required for mattermost auth cookie gathering script
-    'commandt.score' \              # search scoring utility used in custom deoplete filter
-    'feedparser' \                  # rss support for weechat plugin
-    'neovim' \                      # neovim client, required to use python with neovim
-    'pynvim' \                      # neovim client, required to use python with neovim
-    'selenium' \                    # same as click
-    'python-language-server' \      # a better python language server than pyls??
-)
-#   'pynvim' \                      # if your compiled version of neovim does not have python3 support...
-#   'pyls' \                        # python languageserver
 
 # osx specific python 3 packages
 OSXPYTHON3=( \
+    'pync' \                        # required for notification-center.py weechat plugin
 )
 
 #######################
@@ -60,11 +45,6 @@ OSXPYTHON3=( \
 #                     #
 #######################
 
-# install python2 packages
-for i in ${PYTHON2[@]}; do
-  /usr/bin/sudo pip2 install $i
-done
-
 # install python3 packages
 for i in ${PYTHON3[@]}; do
   /usr/bin/sudo pip3 install $i
@@ -72,10 +52,6 @@ done
 
 # osx specific installs
 if [ `uname -s` = 'Darwin' ]; then
-  for i in ${OSXPYTHON2[@]}; do
-    /usr/bin/sudo pip2 install $i
-  done
-
   for i in ${OSXPYTHON3[@]}; do
     /usr/bin/sudo pip3 install $i
   done
