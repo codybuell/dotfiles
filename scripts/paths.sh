a5d8b69e33f6fcbe7a59570adbe3987076ae7cf6
codybuell <cody@codybuell.com>
2020-09-23 20:49:58 -0400

    Clean header, use library, make links more robust
    Updating the header to be consistent with other scripts in this repo.
    Making use of the recently broken out library.sh to reduce code
    duplication. Improving the symlinking function to be a bit more robust
    with paths containing spaces. Setting the script to make paths and link
    from the users home directory in order to simplify configs.

--- scripts/paths.sh
+++ scripts/paths.sh
@@ -6,21 +6,14 @@
 #
 # Author(s): Cody Buell
 #
-# Revisions: 2018.01.17 Initial framework.
-#
 # Requisite:
 #
 # Task List: 
 #
-# Usage: ./paths.sh
+# Usage: make paths
+#        scripts/paths.sh
 
-#########################
-#                       #
-#  Establish Variables  #
-#                       #
-#########################
-
-CONFGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &&  cd ../ && pwd )"
+source scripts/library.sh
 
 ######################
 #                    #
@@ -28,83 +21,52 @@ CONFGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &&  cd ../ && pwd )"
 #                    #
 ######################
 
-readconfig() {
-  CONFIGVARS=()
-  shopt -s extglob
-  configfile="$CONFGDIR/.config"
-  [[ -e $configfile ]] && {
-    tr -d '\r' < $configfile > $configfile.tmp
-    while IFS='= ' read -r lhs rhs; do
-      if [[ ! $lhs =~ ^\ *# && -n $lhs ]]; then
-        rhs="${rhs%%\#*}"    # del in line right comments
-        rhs="${rhs%%*( )}"   # del trailing spaces
-        rhs="${rhs%\"*}"     # del opening string quotes
-        rhs="${rhs#\"*}"     # del closing string quotes
-        export $lhs="$rhs"
-        CONFIGVARS+="$lhs "
-      fi
-    done < $configfile.tmp
-    export CONFIGVARS
-  } || {
-    printf "\033[0;31mno configuration file detected\033[0m\n"
-    exit 1
-  }
-  rm $configfile.tmp
-}
-
-prettyprint() {
-
-  printf "$1" | awk '{file=$1;$1="";printf "  %-40s %s\n", file, $0}' | sed "s/ /,/g;s/\([^,]\),/\1 /g;s/,\([^,]\)/ \1/g;s/^,/ /;s/,/./g";
-
-}
+# Run Paths
+#
+# Create paths as defined by every $PATH.* variable in $CONFGDIR/.config.
 
 runpaths() {
-
   printf "\033[0;34msetting up paths...\033[0m\n"
-
   for p in ${CONFIGVARS[@]}; do
     VAR=$p
     eval VAL=\$$p
     [[ $VAR =~ PATH.* ]] && {
       [[ ! -d $VAL ]] && {
-        prettyprint "  '${VAL}' \033[0;32mcreating path\033[0m\n"
+        prettyprint "  ${VAL}:\033[0;32mcreating path\033[0m\n"
         mkdir -p $VAL
       } || {
-        prettyprint "  '${VAL}' \033[0;32malready exists\033[0m\n"
+        prettyprint "  ${VAL}:\033[0;32malready exists\033[0m\n"
       }
     }
   done
-
 }
 
+# Run Links
+#
+# Create symlinks as defined by every $SYMLINK.* variable in $CONFGDIR/.config.
+
 runlinks() {
-
   printf "\033[0;34msetting up symlinks...\033[0m\n"
-
   LINKMSG="32mcreating symlink"
-
-  [[ -L ~/.todo/tasks ]] && unlink ~/.todo/tasks
-  ln -s "$TasksFolder" ~/.todo/tasks
-
   for c in ${CONFIGVARS[@]}; do
     VAR=$c
     eval VAL=\$$c
     [[ $VAR =~ SYMLINK.* ]] && {
-      DST=`echo $VAL | awk '{print $2}'`
+      DST=`echo $VAL | awk -F: '{print $2}'`
+      SRC=`echo $VAL | awk -F: '{print $1}'`
       [[ -d $DST || -L $DST ]] && {
         [[ -L $DST ]] && {
-          unlink $DST
+          unlink "$DST"
           LINKMSG="33mre-linking symlink"
         } || {
-          prettyprint "  '${DST}' \033[0;33malready exists as a dir\033[0m\n"
+          prettyprint "  ${DST}:\033[0;33malready exists as a dir\033[0m\n"
           continue
         }
       }
-      prettyprint "  '${DST}' \033[0;$LINKMSG\033[0m\n"
-      ln -s $VAL
+      prettyprint "  ${DST}:\033[0;$LINKMSG\033[0m\n"
+      ln -s "$SRC" "$DST"
     }
   done
-
 }
 
 ############
@@ -113,6 +75,7 @@ runlinks() {
 #          #
 ############
 
+cd
 readconfig
 runpaths
 runlinks
