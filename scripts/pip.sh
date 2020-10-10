5b27142472baba4ecd42b7db9e1684d4b57176be
codybuell <cody@codybuell.com>
2020-10-09 22:32:09 -0400

    Add cloud custodian

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -22,16 +22,17 @@
 
 # standard Python 3 packages
 PYTHON3=( \
-    'browsercookie' \               # for getting browser cookies with cookiemonster via cli
-    'click' \                       # required for mattermost auth cookie gathering script
-    'commandt.score' \              # search scoring utility used in custom deoplete filter
-    'feedparser' \                  # rss support for weechat plugin
-    'jmespath-terminal' \           # json manipulation and parsing tool (run as `jpterm`)
-    'neovim' \                      # neovim client, required to use python with neovim
-    'pynvim' \                      # neovim client, required to use python with neovim
-    'selenium' \                    # same as click
-    'websocket-client' \            # required for wee-slack weechat plugin
-    'python-language-server' \      # a better python language server than pyls??
+    'browsercookie' \                  # for getting browser cookies with cookiemonster via cli
+    'click' \                          # required for mattermost auth cookie gathering script
+    'commandt.score' \                 # search scoring utility used in custom deoplete filter
+    'feedparser' \                     # rss support for weechat plugin
+    'jmespath-terminal' \              # json manipulation and parsing tool (run as `jpterm`)
+    'neovim' \                         # neovim client, required to use python with neovim
+    'pynvim' \                         # neovim client, required to use python with neovim
+    'selenium' \                       # same as click
+    'websocket-client' \               # required for wee-slack weechat plugin
+    'python-language-server' \         # a better python language server than pyls??
+    'c7n c7n-azure c7n-gcp c7n-org' \  # cloud custodian bits
 )
 
 # osx specific python 3 packages
