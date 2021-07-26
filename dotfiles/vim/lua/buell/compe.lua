44b9ee937c0a91242ae885ed2a090f0c5c74faad
codybuell <cody@codybuell.com>
2021-07-26 12:18:26 -0400

    Configure compe documentation popup

--- dotfiles/vim/lua/buell/compe.lua
+++ dotfiles/vim/lua/buell/compe.lua
@@ -15,7 +15,14 @@ compe.setup = function()
     max_abbr_width = 100,
     max_kind_width = 100,
     max_menu_width = 100,
-    documentation = true,
+    documentation = {
+      border = 'solid', -- see `:h nvim_open_win`
+      winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+      max_width = 120,
+      min_width = 60,
+      max_height = math.floor(vim.o.lines * 0.3),
+      min_height = 1,
+    };
     source = {
       nvim_lsp = {
         priority = 100,
