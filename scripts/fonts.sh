47fd36ea5025764ee3a407276509f2e2ca906791
codybuell <cody@codybuell.com>
2021-07-26 13:11:14 -0400

    Add md5 checksum testing for make fonts target

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -25,12 +25,27 @@ case `uname -s` in
     echo "to be implemented"
     ;;
   Darwin )
+    which md5 > /dev/null 2>&1
+    [[ $? -gt 0 ]] && {
+      MD5='md5sum'
+      MD5Q='md5sum'
+    } || {
+      MD5='md5'
+      MD5Q='md5 -q'
+    }
     fontloc=/Library/Fonts/
     printf "\033[0;34msetting up fonts...\033[0m\n"
     for font in `find ${CONFGDIR}/assets/fonts -type f -name \*.otf`; do
       fontname=`echo $font | awk -F\/ '{print $NF}'`
       if [ -f "${fontloc}${fontname}" ]; then
-        prettyprint "  ${fontname}:\033[0;33malready installed\033[0m\n"
+        MD5_INSTALLED=$($MD5Q ${fontloc}${fontname})
+        MD5_NEW=$($MD5Q ${font})
+        if [[ "$MD5_INSTALLED" != "$MD5_NEW" ]]; then
+          prettyprint "  ${fontname}:\033[0;32minstalling font\033[0m\n"
+          cp ${font} /Library/Fonts/
+        else
+          prettyprint "  ${fontname}:\033[0;33malready installed\033[0m\n"
+        fi
       else
         prettyprint "  ${fontname}:\033[0;32minstalling font\033[0m\n"
         cp ${font} /Library/Fonts/
