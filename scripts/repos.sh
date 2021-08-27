db2a478a2d303c1b58e281409819d79e8cffd348
codybuell <cody@codybuell.com>
2021-08-27 10:53:18 -0400

    Correct pathing by upstream remote / group

--- scripts/repos.sh
+++ scripts/repos.sh
@@ -21,6 +21,15 @@ source scripts/library.sh
 #                    #
 ######################
 
+# git clone to folder structure $Repos/site/userORgroup/userORgroupN/repo
+function gcl() {
+  CLONEPATH=`echo $1 | sed -E 's/git\@|http(s)?:\/\///;s/\.git$//;s/:/\//g'`
+  git clone $1 $Repos/$CLONEPATH
+  cd $Repos
+  find $Repos -type d -name .git -maxdepth 8 | sed 's/\/\.git$//' | awk -F\/ '{print $NF":"$0}' > .repos
+  cd -
+}
+
 # Clone Repos
 #
 # Clone repos to the $Repos path as defined by every $REPO.* variable in
@@ -34,9 +43,11 @@ clonerepos() {
     [[ $VAR =~ REPO.* ]] && {
       MAKEMODEL=`echo $VAL | sed 's/\// /g;s/\.git//;s/:/ /g' | awk '{print $(NF-1)"/"$NF}'`
       CLONEPATH=`echo $VAL | sed -E 's/git\@|http(s)?:\/\///;s/\.git$//;s/:/\//g'`
+      echo $MAKEMODEL
+      echo $CLONEPATH
       [[ ! -d $CLONEPATH ]] && {
         prettyprint "  ${MAKEMODEL}:\033[0;32mcloning\033[0m\n"
-        git clone $VAL {{ Repos }}/$CLONEPATH
+        gcl $VAL
       } || {
         prettyprint "  ${MAKEMODEL}:\033[0;32malready cloned\033[0m\n"
       }
