9c2915e2cf5b02e2dbead63fc45651c2deed6155
codybuell <cody@codybuell.com>
2022-03-17 10:03:54 -0400

    Include space in regex group

--- dotfiles/vim/lua/buell/todo.lua
+++ dotfiles/vim/lua/buell/todo.lua
@@ -28,7 +28,7 @@ todo.setup = function()
       before = "", -- "fg" or "bg" or empty
       keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
       after = "fg", -- "fg" or "bg" or empty
-      pattern = [[.*<(KEYWORDS) *(\([^\)]*\))?:]], -- pattern or table of patterns, used for highlightng (vim regex)
+      pattern = [[.*<(KEYWORDS)( *\([^\)]*\))?:]], -- pattern or table of patterns, used for highlightng (vim regex)
       comments_only = true, -- uses treesitter to match keywords in comments only
       max_line_len = 400, -- ignore lines longer than this
       exclude = {}, -- list of file types to exclude highlighting
@@ -53,7 +53,7 @@ todo.setup = function()
       },
       -- regex that will be used to match keywords.
       -- don't replace the (KEYWORDS) placeholder
-      pattern = [[\b(KEYWORDS) *(\([^\)]*\))?:]], -- ripgrep regex
+      pattern = [[\b(KEYWORDS)( *\([^\)]*\))?:]], -- ripgrep regex
       -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
     },
   }
