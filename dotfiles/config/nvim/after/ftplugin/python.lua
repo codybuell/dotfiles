249a51507d3520b877aef0e5dc18136cc4c59f3c
codybuell <cody@codybuell.com>
2022-09-29 13:23:23 -0400

    Don't auto indent when typing : in python

--- dotfiles/config/nvim/after/ftplugin/python.lua
+++ dotfiles/config/nvim/after/ftplugin/python.lua
@@ -1,1 +1,2 @@
-vim.opt_local.foldmethod = 'expr'    -- setting expr to allow treesitter to handle folds
+vim.opt_local.foldmethod = 'expr'       -- setting expr to allow treesitter to handle folds
+vim.opt_local.indentkeys:remove("<:>")  -- stop indenting when you type a : anywhere on the line
