c72d4a0c255dee7027afecd7e0d0d473f73b7240
codybuell <cody@codybuell.com>
2022-10-06 12:43:06 -0400

    Dirvish to sort by type first then alphabetical

--- dotfiles/config/nvim/after/plugin/dirvish.lua
+++ dotfiles/config/nvim/after/plugin/dirvish.lua
@@ -7,6 +7,9 @@
 local augroup = buell.util.augroup
 local autocmd = buell.util.autocmd
 
+-- sort dirs then files, dots first then alphabetical for each
+vim.g.dirvish_mode = ':sort | sort ,^.*[^/]$, r'
+
 augroup('BuellDirvish', function()
   autocmd('Filetype', 'dirvish', function()
     vim.keymap.set('n', '!', ':<C-U>Shdo ', {remap = false, nowait = true, buffer = true})
