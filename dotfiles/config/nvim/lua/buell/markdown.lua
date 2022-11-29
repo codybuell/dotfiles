c9834d0896e75326e251fdc1b3323a8371f4926c
codybuell <cody@codybuell.com>
2022-11-28 20:08:24 -0500

    Add codex to wiki list

--- dotfiles/config/nvim/lua/buell/markdown.lua
+++ dotfiles/config/nvim/lua/buell/markdown.lua
@@ -10,6 +10,7 @@ local markdown = {}
 local wikis = {
   notes = '{{ Notes }}',
   journal = '{{ Journal }}',
+  codex = '{{ Codex }}',
 }
 
 -- define link syntaxes, order is important in link_types due to lua's lack of
