682d6c7a6c7c24e07d42fdd6248304a17fd89955
codybuell <cody@codybuell.com>
2022-12-18 15:27:30 -0500

    Tweak ferret search settings

---
+++ dotfiles/config/nvim/after/plugin/ferret.lua
@@ -0,0 +1,4 @@
+vim.g.FerretExecutableArguments = {
+  --   rg = '--vimgrep --no-heading --max-columns 4096 --hidden'
+  rg = '--vimgrep --no-heading --no-config --max-columns 4096 --hidden --glob !.git',
+}
