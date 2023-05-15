c64fb02bc40210d8b0e84e46fd2bd74d95628041
codybuell <cody@codybuell.com>
2023-05-15 13:19:34 -0400

    Update treesitter and support for new nvim lsp

--- dotfiles/config/nvim/lua/buell/lsp.lua
+++ dotfiles/config/nvim/lua/buell/lsp.lua
@@ -43,7 +43,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 -- add cmp capabilities
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- add lsp-status capabilities
 capabilities = vim.tbl_extend('keep', capabilities or {}, lsp_status.capabilities)
