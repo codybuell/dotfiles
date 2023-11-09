a5e6d44aea931840318e31d39cefa94f9c067718
codybuell <cody@codybuell.com>
2023-11-09 09:07:38 -0500

    Only enable mappings on markdown files

--- dotfiles/config/nvim/after/plugin/autolist.nvim.lua
+++ dotfiles/config/nvim/after/plugin/autolist.nvim.lua
@@ -4,6 +4,9 @@
 --                                                                            --
 --------------------------------------------------------------------------------
 
+local augroup = buell.util.augroup
+local autocmd = buell.util.autocmd
+
 local list_patterns = {
     neorg_1 = "%-",
     neorg_2 = "%-%-",
@@ -71,25 +74,30 @@ local config = {
 
 require("autolist").setup(config)
 
-vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
-vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
--- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
-vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
-vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
-vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
-vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+augroup('BuellAutolist', function()
+  autocmd('Filetype', 'markdown', function()
+    vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+    vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+    -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
+    vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+    vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+    vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+    vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+    vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
 
--- cycle list types with dot-repeat
-vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
-vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
+    -- cycle list types with dot-repeat
+    vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
+    vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
 
--- if you don't want dot-repeat
--- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
--- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
+    -- if you don't want dot-repeat
+    -- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
+    -- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
 
--- functions to recalculate list on edit
-vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
-vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
-vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
-vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+    -- functions to recalculate list on edit
+    vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+    vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+    vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+    vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+  end)
+end)
+
