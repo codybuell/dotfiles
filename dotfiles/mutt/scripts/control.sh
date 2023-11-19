dd43b5f2de7bf70e954d6e9e674e412ae68947f2
codybuell <cody@codybuell.com>
2023-11-19 16:47:34 -0500

    Address shellcheck findings

--- dotfiles/mutt/scripts/control.sh
+++ dotfiles/mutt/scripts/control.sh
@@ -4,7 +4,7 @@
 # Helpers #
 ###########
 
-function usage() {
+usage() {
   echo "Commands:"
   echo "  exit   - exit this control loop"
   echo "  help   - show this help"
@@ -13,11 +13,11 @@ function usage() {
   echo "  sync   - force an immediate email sync"
 }
 
-function signal() {
+signal() {
   SIGNAL=$1
   PIDFILE=$2
   if [ -f "$PIDFILE" ]; then
-    PID=`cat "$PIDFILE"`
+    PID=$(cat "$PIDFILE")
     kill "$SIGNAL" "$PID"
     echo "Sent $SIGNAL signal to PID $PID ($PIDFILE)"
   else
@@ -25,12 +25,12 @@ function signal() {
   fi
 }
 
-function pause() {
+pause() {
   PIDFILE=$1
   signal "-STOP" "$PIDFILE"
 }
 
-function resume() {
+resume() {
   PIDFILE=$1
   signal "-CONT" "$PIDFILE"
 }
@@ -39,17 +39,17 @@ function resume() {
 # Actions #
 ###########
 
-function pausing() {
+pausing() {
   pause "$HOME/.mutt/tmp/sync-home.pid"
   pause "$HOME/.mutt/tmp/sync-work.pid"
 }
 
-function resuming() {
+resuming() {
   resume "$HOME/.mutt/tmp/sync-home.pid"
   resume "$HOME/.mutt/tmp/sync-work.pid"
 }
 
-function syncing() {
+syncing() {
   "$HOME/.mutt/scripts/download.sh" home
   "$HOME/.mutt/scripts/download.sh" work
 }
@@ -61,7 +61,7 @@ function syncing() {
 if [ $# -eq 1 ]; then
   case $1 in
     home|work )
-      "$HOME/.mutt/scripts/sync.sh" "$1" || reattach-to-user-namespace terminal-notifier -title mutt -message "~/.mutt/scripts/sync.sh ($1) exited" Enter
+      "$HOME/.mutt/scripts/sync.sh" "$1" || reattach-to-user-namespace terminal-notifier -title mutt -message "$HOME/.mutt/scripts/sync.sh ($1) exited" Enter
       exit 0
       ;;
     pause|paus|pau|pa|p )
@@ -112,6 +112,6 @@ while true; do
       echo "Valid commands: exit, help, pause, resume, sync"
       ;;
   esac
-  /bin/echo -n "> "
-  read COMMAND
+  /bin/echo "> "
+  read -r COMMAND
 done
--- dotfiles/mutt/scripts/tmux_window_name.sh
+++ dotfiles/mutt/scripts/tmux_window_name.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-WINDOWNAME=`tmux display-message -t HOME:1 -p '#W'`
+# WINDOWNAME=$(tmux display-message -t HOME:1 -p '#W')
 
 INBOXHOME="$HOME/.mail/Home/Home/new/"
 INBOXWORK="$HOME/.mail/Work/Work/new/"
 
-NEWHOME=`find $INBOXHOME -type f | wc -l | sed 's/ //g'`
-NEWWORK=`find $INBOXWORK -type f | wc -l | sed 's/ //g'`
+NEWHOME=$(find "$INBOXHOME" -type f | wc -l | sed 's/ //g')
+NEWWORK=$(find "$INBOXWORK" -type f | wc -l | sed 's/ //g')
 
 tmux rename-window -t HOME:1 "H:$NEWHOME W:$NEWWORK "
 
