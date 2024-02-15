86063ce0a7d04a9649b67f5e1f81dbcf91d778f6
codybuell <cody@codybuell.com>
2024-02-15 09:30:06 -0500

    Improved tabline generation, single sizing flow
    Instead of incrementally shortening every tab name until the tabline
    fits we are doing math to determine how many characters the tabline is
    over then determining how many chars to remove from each tab. Result is
    that there should be no leftover unused spaces with overly crunched tab
    names.

--- dotfiles/config/nvim/lua/buell/tabline/label.lua
+++ dotfiles/config/nvim/lua/buell/tabline/label.lua
@@ -1,7 +1,27 @@
-local label = function(n)
+local label = function(n, trim)
   local buflist = vim.fn.tabpagebuflist(n)
-  local winnr = vim.fn.tabpagewinnr(n)
-  return n .. ': ' .. vim.fn.pathshorten(vim.fn.fnamemodify(vim.fn.bufname(buflist[winnr]), ':~:.'))
+  local winnr   = vim.fn.tabpagewinnr(n)
+
+  local full_title = vim.fn.pathshorten(vim.fn.fnamemodify(vim.fn.bufname(buflist[winnr]), ':~:.'))
+
+  if trim == 0 then
+    return n .. ': ' .. full_title
+  end
+
+  local filename = vim.fn.pathshorten(vim.fn.fnamemodify(vim.fn.bufname(buflist[winnr]), ':t'))
+  local filepath = full_title:sub(0, #full_title - #filename)
+
+  if trim <= #filepath then
+    print('trimpath only ' .. trim .. '(' .. #filepath .. ')' .. filepath)
+    filepath = '…' .. filepath:sub(trim + 2)
+  else
+    trim = trim - #filepath
+    print('trimfile ' .. trim .. '(' .. #filename .. ')' .. filename)
+    filepath = ''
+    filename = filename:sub(0, #filename - trim - 2) .. '…'
+  end
+
+  return n .. ': ' .. filepath .. filename
 end
 
 return label
--- dotfiles/config/nvim/lua/buell/tabline/render.lua
+++ dotfiles/config/nvim/lua/buell/tabline/render.lua
@@ -2,23 +2,55 @@
 -- counts, the modified flag, or the close widget.
 local render = function()
   local gutter_width = buell.util.gutter_width()
-  -- local window_width = vim.o.columns
-  local line    = string.rep(' ', gutter_width)
-  local current = vim.api.nvim_tabpage_get_number(0)
+  local window_width = vim.o.columns
+  local avail_width  = window_width - gutter_width
+  local line_start   = string.rep(' ', gutter_width)
+  local current_tab  = vim.api.nvim_tabpage_get_number(0)
+  local tab_count    = vim.fn.tabpagenr('$')
 
+  -- determine full size tab bar and build compression array
+  local width = 0
+  local trim_array = {}
+  for i = 1, tab_count do
+    width = width + #buell.tabline.label(i, 0) + 2
+    table.insert(trim_array, 0)
+  end
+
+  -- if the tabbar is too wide determine how many chars to remove from each tab
+  if width > avail_width then
+    local over = width - avail_width
+    local per_tab, slop
+    if over >= tab_count then
+      per_tab = math.floor(over / tab_count)
+      slop = over % tab_count
+    else
+      per_tab = 0
+      slop = over % tab_count
+    end
+    for i = 1, tab_count do
+      trim_array[i] = trim_array[i] + per_tab
+    end
+    for i = 1, slop do
+      trim_array[i] = trim_array[i] + 1
+    end
+  end
+
+  -- build the tabline with per tab trims
+  local line = ''
   for i = 1, vim.fn.tabpagenr('$') do
-    if i == current then
+    if i == current_tab then
       line = line .. '%#TabLineSel#'
     else
       line = line .. '%#TabLine#'
     end
     line = line .. '%' .. i .. 'T' -- Starts mouse click target region.
-    line = line .. ' %{v:lua.buell.tabline.label(' .. i .. ')} '
+    line = line .. ' %{v:lua.buell.tabline.label(' .. i .. ', ' .. trim_array[i] .. ')} '
   end
+
 
   line = line .. '%#TabLineFill#'
   line = line .. '%T' -- Ends mouse click target region(s).
-  return line
+  return line_start .. line
 end
 
 return render
