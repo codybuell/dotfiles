c58c8654f8f733dcd3ab6d8a2863ad24c8db1ebb
codybuell <cody@codybuell.com>
2024-03-06 15:29:17 -0500

    For now just show filename in the tabline

--- dotfiles/config/nvim/lua/buell/tabline/label.lua
+++ dotfiles/config/nvim/lua/buell/tabline/label.lua
@@ -2,24 +2,37 @@ local label = function(n, trim)
   local buflist = vim.fn.tabpagebuflist(n)
   local winnr   = vim.fn.tabpagewinnr(n)
 
-  local full_title = vim.fn.pathshorten(vim.fn.fnamemodify(vim.fn.bufname(buflist[winnr]), ':~:.'))
+  local full_title = vim.fn.pathshorten(vim.fn.fnamemodify(vim.fn.bufname(buflist[winnr]), ':t'))
 
   if trim == 0 then
     return n .. ': ' .. full_title
-  end
-
-  local filename = vim.fn.pathshorten(vim.fn.fnamemodify(vim.fn.bufname(buflist[winnr]), ':t'))
-  local filepath = full_title:sub(0, #full_title - #filename)
-
-  if trim <= #filepath then
-    filepath = '…' .. filepath:sub(trim + 2)
   else
-    trim = trim - #filepath
-    filepath = ''
-    filename = filename:sub(0, #filename - trim - 2) .. '…'
+    return n .. ': ' .. full_title:sub(0, #full_title - trim - 1) .. '…'
   end
+  ------------------------------------
+  -- VERSION THAT INCLUDES FILEPATH --
+  ------------------------------------
+  -- local buflist = vim.fn.tabpagebuflist(n)
+  -- local winnr   = vim.fn.tabpagewinnr(n)
 
-  return n .. ': ' .. filepath .. filename
+  -- local full_title = vim.fn.pathshorten(vim.fn.fnamemodify(vim.fn.bufname(buflist[winnr]), ':~:.'))
+
+  -- if trim == 0 then
+  --   return n .. ': ' .. full_title
+  -- end
+
+  -- local filename = vim.fn.pathshorten(vim.fn.fnamemodify(vim.fn.bufname(buflist[winnr]), ':t'))
+  -- local filepath = full_title:sub(0, #full_title - #filename)
+
+  -- if trim <= #filepath then
+  --   filepath = '…' .. filepath:sub(trim + 2)
+  -- else
+  --   trim = trim - #filepath
+  --   filepath = ''
+  --   filename = filename:sub(0, #filename - trim - 2) .. '…'
+  -- end
+
+  -- return n .. ': ' .. filepath .. filename
 end
 
 return label
