443f6a93124fadaa223075129708f18af807cc07
codybuell <cody@codybuell.com>
2024-07-08 08:34:29 -0400

    Add maps to sort alphabetically and by length

--- dotfiles/config/nvim/plugin/mappings/visual.lua
+++ dotfiles/config/nvim/plugin/mappings/visual.lua
@@ -5,3 +5,5 @@
 --------------------------------------------------------------------------------
 
 vim.keymap.set('v', '<Leader>rl', ':VtrSendLinesToRunner<CR>', {remap = false})
+vim.keymap.set('v', '<Leader>sl', ':\'<,\'>!awk \'{print length(), $0 | "sort -n | cut -d\\" \\" -f2-" }\'<CR>', {remap = false})
+vim.keymap.set('v', '<Leader>sa', ':\'<,\'>!sort<CR>', {remap = false})
