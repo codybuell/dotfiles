98b97c41054dc07537ff3f3b533919b82b06aeec
codybuell <cody@codybuell.com>
2024-07-18 22:17:34 -0400

    Adjust todo-comments colors, fix char for FIX

--- dotfiles/config/nvim/after/plugin/todo-comments.nvim.lua
+++ dotfiles/config/nvim/after/plugin/todo-comments.nvim.lua
@@ -6,7 +6,7 @@ todo.setup {
   -- keywords recognized as todo comments
   keywords = {
     FIX = {
-      icon = " ", -- icon used for the sign, and in search results
+      icon = "✓ ", -- icon used for the sign, and in search results
       color = "error", -- can be a hex color, or a named color (see below)
       alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
       -- signs = false, -- configure signs for some keywords individually
@@ -24,7 +24,7 @@ todo.setup {
   -- * after: highlights after the keyword (todo text)
   highlight = {
     before = "", -- "fg" or "bg" or empty
-    keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+    keyword = "fg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
     after = "fg", -- "fg" or "bg" or empty
     pattern = [[.*<(KEYWORDS)( *\([^\)]*\))?:]], -- pattern or table of patterns, used for highlightng (vim regex)
     comments_only = true, -- uses treesitter to match keywords in comments only
