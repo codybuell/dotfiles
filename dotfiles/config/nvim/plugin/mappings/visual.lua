17b0aa93a524a439ad27ca56f531c374d182c7d9
codybuell <cody@codybuell.com>
2024-07-25 22:55:17 -0400

    Allow tmux navigator to work with visual selection

--- dotfiles/config/nvim/plugin/mappings/visual.lua
+++ dotfiles/config/nvim/plugin/mappings/visual.lua
@@ -7,3 +7,8 @@
 vim.keymap.set('v', '<Leader>rl', ':VtrSendLinesToRunner<CR>', {remap = false})
 vim.keymap.set('v', '<Leader>sl', ':\'<,\'>!awk \'{print length(), $0 | "sort -n | cut -d\\" \\" -f2-" }\'<CR>', {remap = false})
 vim.keymap.set('v', '<Leader>sa', ':\'<,\'>!sort<CR>', {remap = false})
+
+vim.keymap.set('v', '<C-h>', ':<C-U>TmuxNavigateLeft<CR>', {remap=false, silent=true})
+vim.keymap.set('v', '<C-j>', ':<C-U>TmuxNavigateDown<CR>', {remap=false, silent=true})
+vim.keymap.set('v', '<C-k>', ':<C-U>TmuxNavigateUp<CR>', {remap=false, silent=true})
+vim.keymap.set('v', '<C-l>', ':<C-U>TmuxNavigateRight<CR>', {remap=false, silent=true})
