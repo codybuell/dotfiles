0b3f9c0a767ed78f010d89b7ec4e1f8f4194ecd5
codybuell <cody@codybuell.com>
2024-08-07 15:24:35 -0400

    Disable auto scroll for markdown preview

---
+++ dotfiles/config/nvim/after/plugin/markdown-preview.nvim.lua
@@ -0,0 +1,14 @@
+vim.g.mkdp_preview_options = {
+  mkit = {},
+  katex = {},
+  uml = {},
+  maid = {},
+  disable_sync_scroll = 1,
+  sync_scroll_type = 'middle',
+  hide_yaml_meta = 1,
+  sequence_diagrams = {},
+  flowchart_diagrams = {},
+  content_editable = false,
+  disable_filename = 0,
+  toc = {}
+}
