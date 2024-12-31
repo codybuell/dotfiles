d2757d254f4b76192b087fbde5fd5ebd3c57f8ea
codybuell <cody@codybuell.com>
2024-12-31 14:40:59 -0500

    Control what files have copilot enabled

--- dotfiles/config/nvim/after/plugin/copilot.vim.lua
+++ dotfiles/config/nvim/after/plugin/copilot.vim.lua
@@ -3,4 +3,21 @@ vim.keymap.set('i', '<C-o>', 'copilot#Accept("\\<CR>")', {
   expr = true,
   replace_keycodes = false
 })
+
 vim.g.copilot_no_tab_map = true
+
+vim.g.copilot_filetypes = {
+  ['*'] = false,
+  markdown = false,
+  python = true,
+  go = true,
+  javascript = true,
+  typescript = true,
+  rust = true,
+  lua = true,
+  vim = true,
+  sh = true,
+  yaml = true,
+  json = true,
+  toml = true,
+}
