43d7eb79460243fea31ede98179b28ac7b3a13d3
codybuell <cody@codybuell.com>
2025-01-10 14:29:44 -0500

    Tune up copilotchat

--- dotfiles/config/nvim/after/plugin/copilotchat.nvim.lua
+++ dotfiles/config/nvim/after/plugin/copilotchat.nvim.lua
@@ -1,161 +1,72 @@
+local copilot_instructions = require("CopilotChat.prompts").COPILOT_INSTRUCTIONS
+
+local custom_instructions = [[
+You should also:
+* Keep responses as concise as possible. Be pithy unless asked for more context.
+* When providing code changes only show the relevant code plus a few extra lines above and below for context.
+* When providing code changes, show the relevant code in a `diff` format using Markdown. Ensure the syntax highlighting of the actual language is preserved. For example:
+
+```diff
+```python
+- old line of code
++ new line of code
+```
+```
+]]
+
 require("CopilotChat").setup {
-  debug = false, -- Enable debug logging
-  proxy = nil, -- [protocol://]host[:port] Use this proxy
-  allow_insecure = false, -- Allow insecure server connections
-
-  system_prompt = string.format(
-[[You are an AI programming assistant.
-When asked for your name, you must respond with "GitHub Copilot".
-Follow the user's requirements carefully & to the letter.
-Follow Microsoft content policies.
-Avoid content that violates copyrights.
-If you are asked to generate content that is harmful, hateful, racist, sexist, lewd, violent, or completely irrelevant to software engineering, only respond with "Sorry, I can't assist with that."
-Keep your answers short and impersonal.
-You can answer general programming questions and perform the following tasks:
-* Ask a question about the files in your current workspace
-* Explain how the code in your active editor works
-* Generate unit tests for the selected code
-* Propose a fix for the problems in the selected code
-* Scaffold code for a new workspace
-* Create a new Jupyter Notebook
-* Find relevant code to your query
-* Propose a fix for the a test failure
-* Ask questions about Neovim
-* Generate query parameters for workspace search
-* Ask how to do something in the terminal
-* Explain what just happened in the terminal
-You use the GPT-4 version of OpenAI's GPT models.
-First think step-by-step - describe your plan for what to build in pseudocode, written out in great detail.
-Then output the code in a single code block. This code block should not contain line numbers (line numbers are not necessary for the code to be understood, they are in format number: at beginning of lines).
-Use the same indentation level as any soruce code in the users input when providing code examples, updates, or modifications.
-When providing updated code, code changes, or code adjustments only show the portions of the code that were modified with a few lines before and after for context.
-Show code changes in an inline diff.
-Minimize any other prose.
-Use Markdown formatting in your answers.
-Make sure to include the programming language name at the start of the Markdown code blocks.
-Avoid wrapping the whole response in triple backticks.
-The user works in an IDE called Neovim which has a concept for editors with open files, integrated unit test support, an output pane that shows the output of running the code as well as an integrated terminal.
-The user is working on a %s machine. Please respond with system specific commands if applicable.
-The active document is the source code the user is looking at right now.
-You can only give one reply for each conversation turn.
-]],
-  vim.loop.os_uname().sysname
-),
-
-  model = 'gpt-4o', -- GPT model to use, 'gpt-3.5-turbo', 'gpt-4', or 'gpt-4o'
-  temperature = 0.1, -- GPT temperature
-
-  question_header = '## User ', -- Header to use for user questions
-  answer_header = '## Copilot ', -- Header to use for AI answers
-  error_header = '## Error ', -- Header to use for errors
-  separator = '───', -- Separator to use in chat
-
-  show_folds = true, -- Shows folds for sections in chat
-  show_help = false, -- Shows help message as virtual lines when waiting for user input
-  auto_follow_cursor = false, -- Auto-follow cursor in chat
-  auto_insert_mode = false, -- Automatically enter insert mode when opening window and if auto follow cursor is enabled on new prompt
-  clear_chat_on_new_prompt = false, -- Clears chat on every new prompt
-  highlight_selection = true, -- Highlight selection in the source buffer when in the chat window
-
-  context = 'buffers', -- Default context to use, 'buffers', 'buffer' or none (can be specified manually in prompt via @).
-  history_path = vim.fn.stdpath('data') .. '/copilotchat_history', -- Default path to stored history
-  callback = nil, -- Callback to use when ask response is received
-
-  -- default selection (visual or line)
-  selection = function(source)
-    return require('CopilotChat.select').visual(source) or require('CopilotChat.select').line(source)
-  end,
-
-  -- default prompts
-  prompts = {
-    Explain = {
-      prompt = '/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text.',
-    },
-    Review = {
-      prompt = '/COPILOT_REVIEW Review the selected code.',
-      callback = function(response, source)
-        -- see config.lua for implementation
-      end,
-    },
-    Fix = {
-      prompt = '/COPILOT_GENERATE There is a problem in this code. Rewrite the code to show it with the bug fixed.',
-    },
-    Optimize = {
-      prompt = '/COPILOT_GENERATE Optimize the selected code to improve performance and readablilty.',
-    },
-    Docs = {
-      prompt = '/COPILOT_GENERATE Please add documentation comment for the selection.',
-    },
-    Tests = {
-      prompt = '/COPILOT_GENERATE Please generate tests for my code.',
-    },
-    FixDiagnostic = {
-      prompt = 'Please assist with the following diagnostic issue in file:',
-      selection = require('CopilotChat.select').diagnostics,
-    },
-    Commit = {
-      prompt = 'Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.',
-      selection = require('CopilotChat.select').gitdiff,
-    },
-    CommitStaged = {
-      prompt = 'Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.',
-      selection = function(source)
-        return require('CopilotChat.select').gitdiff(source, true)
-      end,
-    },
-  },
-
-  -- default window options
-  window = {
-    layout = 'vertical', -- 'vertical', 'horizontal', 'float', 'replace'
-    width = 0.5, -- fractional width of parent, or absolute width in columns when > 1
-    height = 0.5, -- fractional height of parent, or absolute height in rows when > 1
-    -- Options below only apply to floating windows
-    relative = 'editor', -- 'editor', 'win', 'cursor', 'mouse'
-    border = 'single', -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
-    row = nil, -- row position of the window, default is centered
-    col = nil, -- column position of the window, default is centered
-    title = 'Copilot Chat', -- title of chat window
-    footer = nil, -- footer of chat window
-    zindex = 1, -- determines if window is on top or below other floating windows
-  },
-
-  -- default mappings
+  -- context = 'buffers',
+  system_prompt = copilot_instructions .. custom_instructions,
+  show_help = false,
   mappings = {
     complete = {
-      detail = 'Use @<Tab> or /<Tab> for options.',
-      -- insert ='<Tab>',
-      insert ='',
+      insert = '<Tab>',
     },
     close = {
+      -- normal = 'q',
       normal = 'gq',
-      insert = '<C-c>'
+      insert = '<C-c>',
     },
     reset = {
-      normal ='<localleader>3',
-      insert = '<localleader>3'
+      normal = '<C-l>',
+      insert = '<C-l>',
     },
     submit_prompt = {
+      -- normal = '<CR>',
+      -- insert = '<C-s>',
       normal = '<C-s>',
       insert = '<C-s>'
     },
+    toggle_sticky = {
+      detail = 'Makes line under cursor sticky or deletes sticky line.',
+      normal = 'gr',
+    },
     accept_diff = {
-      -- normal = '<C-y>',
-      -- insert = '<C-y>'
-      normal ='',
-      insert = ''
+      normal = '<C-y>',
+      insert = '<C-y>',
+    },
+    jump_to_diff = {
+      normal = 'gj',
+    },
+    quickfix_diffs = {
+      -- normal = 'gq',
+      normal = '',
     },
     yank_diff = {
       normal = 'gy',
+      register = '"',
     },
     show_diff = {
-      normal = 'gd'
+      normal = 'gd',
     },
     show_info = {
-      normal = 'gp'
+      normal = 'gi',
     },
     show_context = {
-      normal = 'gs'
+      normal = 'gc',
+    },
+    show_help = {
+      normal = 'gh',
     },
   },
 }
