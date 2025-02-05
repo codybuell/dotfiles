c493d5ab6413863b99410a80ab592d6069b0227b
codybuell <cody@codybuell.com>
2025-02-05 16:05:24 -0500

    Stub out a config for mini.diff

---
+++ dotfiles/config/nvim/after/plugin/mini.diff.lua
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------------------
+--                                                                            --
+--  Mini.Diff                                                                 --
+--                                                                            --
+--  https://github.com/echasnovski/mini.diff                                  --
+--                                                                            --
+--------------------------------------------------------------------------------
+
+--local has_minidiff, minidiff = pcall(require, 'mini.diff')
+--if has_minidiff then
+
+--  -------------
+--  --  Setup  --
+--  -------------
+
+--  minidiff.setup()
+
+--end
