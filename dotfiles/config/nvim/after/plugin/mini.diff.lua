d81c86c54ced1acc8f3066a62fb68c91016b4008
codybuell <cody@codybuell.com>
2025-02-05 17:28:56 -0500

    Enable and configure mini.diff

--- dotfiles/config/nvim/after/plugin/mini.diff.lua
+++ dotfiles/config/nvim/after/plugin/mini.diff.lua
@@ -6,13 +6,38 @@
 --                                                                            --
 --------------------------------------------------------------------------------
 
---local has_minidiff, minidiff = pcall(require, 'mini.diff')
---if has_minidiff then
+local has_minidiff, minidiff = pcall(require, 'mini.diff')
+if has_minidiff then
 
---  -------------
---  --  Setup  --
---  -------------
+  -------------
+  --  Setup  --
+  -------------
 
---  minidiff.setup()
+  minidiff.setup()
 
---end
+  --------------
+  --  Colors  --
+  --------------
+
+  local pinnacle = require('wincent.pinnacle')
+
+  pinnacle.set('MiniDiffSignAdd', {
+    bg = pinnacle.bg('SignColumn'),
+    fg = pinnacle.fg('DiffAdd'),
+    -- bold = true,
+    italic = true,
+  })
+  pinnacle.set('MiniDiffSignChange', {
+    bg = pinnacle.bg('SignColumn'),
+    fg = pinnacle.fg('DiffText'),
+    -- bold = true,
+    italic = true,
+  })
+  pinnacle.set('MiniDiffSignDelete', {
+    bg = pinnacle.bg('SignColumn'),
+    fg = pinnacle.fg('DiffDelete'),
+    -- bold = true,
+    italic = true,
+  })
+
+end
