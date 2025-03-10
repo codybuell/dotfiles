c5783dfc739b175096f0872b1571f4e6a3de3509
codybuell <cody@codybuell.com>
2025-03-10 16:20:09 -0400

    Refine markdown folding expression
    Fixes issue where two spaces under a line would cause it to be
    considered a header... 🤷

--- dotfiles/config/nvim/lua/buell/fold/markdown.lua
+++ dotfiles/config/nvim/lua/buell/fold/markdown.lua
@@ -1,5 +1,5 @@
 -- fold only headers
-local markdown = function ()
+local markdown = function()
   -- To debug this, set these to show the fold level in the gutter:
   --    :set statuscolumn=%l\ %{foldlevel(v:lnum)}
   --    :set numberwidth=6
@@ -8,7 +8,7 @@ local markdown = function ()
   -- lines[2] current line    header
   -- lines[3]                 ======
   -- lines[4]                 ======
-  local lnum  = vim.v.lnum
+  local lnum = vim.v.lnum
   local lines = vim.fn.getline(lnum - 1, lnum + 2)
 
   -- if lnum is 1, insert "" at the start of the lines table
@@ -17,11 +17,8 @@ local markdown = function ()
   end
 
   -- pad the end of the table as needed
-  local length = #lines
-  if length > 4 then
-    for i = 1, 4 - length do
-      table.insert(lines, length + i, "")
-    end
+  while #lines < 4 do
+    table.insert(lines, "")
   end
 
   -- handle last line
@@ -31,8 +28,8 @@ local markdown = function ()
 
   -- detect frontmatter
   local is_in_frontmatter = false
-  if vim.fn.getline(1):match('^---$') then
-    for i = 2, vim.fn.line('$') do
+  if lnum <= 20 and vim.fn.getline(1):match('^---$') then
+    for i = 2, math.min(vim.fn.line('$'), 20) do
       if vim.fn.getline(i):match('^---$') then
         if lnum <= i then
           is_in_frontmatter = true
@@ -45,22 +42,24 @@ local markdown = function ()
   -- handle header folding
   if not is_in_frontmatter then
     if lines[2]:match('^# .*$') and lines[1]:match('^%s*$') and lines[3]:match('^%s*$') then
-      return ">1"      -- begin a fold of level one here
+      return ">1"
     elseif lines[2]:match('^## .*$') and lines[1]:match('^%s*$') and lines[3]:match('^%s*$') then
-      return ">2"      -- begin a fold of level two here
+      return ">2"
     elseif lines[2]:match('^### .*$') and lines[1]:match('^%s*$') and lines[3]:match('^%s*$') then
-      return ">3"      -- begin a fold of level three here
+      return ">3"
     elseif lines[2]:match('^#### .*$') and lines[1]:match('^%s*$') and lines[3]:match('^%s*$') then
-      return ">4"      -- begin a fold of level four here
-    elseif lines[2] ~= '' and lines[3]:match('^===*$') and lines[4]:match('^%s*$') then
-      return ">1"      -- h1 fold with underscores (2 or more equals for underscores)
-    elseif lines[2] ~= '' and lines[3]:match('^---*$') and lines[4]:match('^%s*$') then
-      return ">2"      -- h2 fold with underscores (2 or more dashes for underscores)
+      return ">4"
+    elseif lines[2] ~= '' and lines[3]:match('^=+$') and lines[4]:match('^%s*$') then
+      return ">1"
+    elseif lines[2] ~= '' and lines[3]:match('^-+$') and lines[4]:match('^%s*$') then
+      return ">2"
     end
   end
 
-  if vim.fn.foldlevel(lnum - 1) ~= -1 then
-    return vim.fn.foldlevel(lnum - 1)
+  -- use previous fold level if available
+  local prev_level = vim.fn.foldlevel(lnum - 1)
+  if prev_level ~= -1 then
+    return prev_level
   end
 
   return "="
