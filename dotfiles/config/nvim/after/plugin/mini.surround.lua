493d99717d150478d7bd5a63a6f5dd185eac27b5
codybuell <cody@codybuell.com>
2025-07-01 14:45:33 -0400

    Note some common actions

--- dotfiles/config/nvim/after/plugin/mini.surround.lua
+++ dotfiles/config/nvim/after/plugin/mini.surround.lua
@@ -4,6 +4,10 @@
 --                                                                            --
 --  https://github.com/echasnovski/mini.diff                                  --
 --                                                                            --
+--  saiW-      surround the Word with -                                       --
+--  Vsa_       surround the whole line with _                                 --
+--  sa__       surround the whole line with _ (this is right, dot rep works)  --
+--                                                                            --
 --------------------------------------------------------------------------------
 
 local has_minisurround, minisurround = pcall(require, 'mini.surround')
