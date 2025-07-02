c4f9132ee2801cdf3a2c1cdf4bbc4bc5cac7f5f6
codybuell <cody@codybuell.com>
2025-07-02 09:23:16 -0400

    Dynamically determine comment string for svelte

--- dotfiles/config/nvim/snippets/luasnip/svelte.lua
+++ dotfiles/config/nvim/snippets/luasnip/svelte.lua
@@ -1,12 +1,12 @@
--- local ls = require"luasnip"
--- local s = ls.snippet
--- local sn = ls.snippet_node
+local ls = require"luasnip"
+local s = ls.snippet
+local sn = ls.snippet_node
 -- local isn = ls.indent_snippet_node
 -- local t = ls.text_node
 -- local i = ls.insert_node
 -- local f = ls.function_node
 -- local c = ls.choice_node
--- local d = ls.dynamic_node
+local d = ls.dynamic_node
 -- local r = ls.restore_node
 -- local events = require("luasnip.util.events")
 -- local ai = require("luasnip.nodes.absolute_indexer")
@@ -15,11 +15,29 @@
 -- local lambda = require("luasnip.extras").l
 -- local postfix = require("luasnip.extras.postfix").postfix
 
+-----------------------------
+--  Dynamic Box Functions  --
+-----------------------------
+
+local function svelte_box_snippet()
+  return s("box", d(1, function(_, _, _)
+    local c_str = buell.sniputils.svelte_comment_style()
+    return sn(nil, buell.sniputils.box(c_str).nodes)
+  end, {}))
+end
+
+local function svelte_bbox_snippet()
+  return s("bbox", d(1, function(_, _, _)
+    local c_str = buell.sniputils.svelte_comment_style()
+    return sn(nil, buell.sniputils.bbox(c_str).nodes)
+  end, {}))
+end
+
 -----------------
 --   Snippets  --
 -----------------
 
 return {
-  buell.sniputils.box("//"),
-  buell.sniputils.bbox("//")
+  svelte_box_snippet(),
+  svelte_bbox_snippet(),
 }
