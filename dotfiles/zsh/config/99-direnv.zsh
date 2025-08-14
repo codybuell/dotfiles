0330580d72b71915109e5409103b3bdfff5da318
codybuell <cody@codybuell.com>
2025-08-14 16:52:43 -0600

    Add direnv module to zsh config

---
+++ dotfiles/zsh/config/99-direnv.zsh
@@ -0,0 +1,12 @@
+################################################################################
+##                                                                            ##
+##  DirEnv                                                                    ##
+##                                                                            ##
+##  Automatic loading of directory specific environments as configured        ##
+##  in `.envrc` files.                                                        ##
+##                                                                            ##
+##  Dependencies: direnv                                                      ##
+##                                                                            ##
+################################################################################
+
+eval "$(direnv hook zsh)"
