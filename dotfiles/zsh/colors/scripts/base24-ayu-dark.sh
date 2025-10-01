733f03a7740d50cc0ccfb6dee1fc9f181476fcff
codybuell <cody@codybuell.com>
2025-10-01 08:58:22 -0400

    Update tinted theme submodules and files

--- dotfiles/zsh/colors/scripts/base24-ayu-dark.sh
+++ dotfiles/zsh/colors/scripts/base24-ayu-dark.sh
@@ -1,54 +1,59 @@
 #!/usr/bin/env sh
 # tinted-shell (https://github.com/tinted-theming/tinted-shell)
-# Scheme name: Ayu Dark 
-# Scheme author: Jamy Golden (https://github.com/JamyGolden)
+# Scheme name: Ayu Dark
+# Scheme author: Tinted Theming (https://github.com/tinted-theming), Ayu Theme (https://github.com/ayu-theme)
 # Template author: Tinted Theming (https://github.com/tinted-theming)
 export BASE24_THEME="ayu-dark"
 
-color00="0f/13/1a" # Base 00 - Black
+color00="0b/0e/14" # Base 00 - Black
 color01="f0/71/78" # Base 08 - Red
 color02="aa/d9/4c" # Base 0B - Green
-color03="f2/96/68" # Base 09 - Yellow
+color03="ff/b4/54" # Base 0A - Yellow
 color04="59/c2/ff" # Base 0D - Blue
-color05="6c/59/80" # Base 0E - Magenta
+color05="d2/a6/ff" # Base 0E - Magenta
 color06="95/e6/cb" # Base 0C - Cyan
-color07="bf/e1/cf" # Base 06 - White
-color08="20/22/29" # Base 02 - Bright Black
-color09="d9/57/57" # Base 12 - Bright Red
+color07="e6/e1/cf" # Base 05 - White
+color08="3e/4b/59" # Base 03 - Bright Black
+color09="f2/6d/78" # Base 12 - Bright Red
 color10="7f/d9/62" # Base 14 - Bright Green
-color11="ff/8f/40" # Base 13 - Bright Yellow
-color12="40/9f/ff" # Base 16 - Bright Blue
-color13="d2/a6/ff" # Base 17 - Bright Magenta
-color14="95/e6/cb" # Base 15 - Bright Cyan
-color15="f3/f4/f5" # Base 07 - Bright White
-color16="f2/96/68" # Base 09
-color17="e6/b6/73" # Base 0F
-color18="12/21/32" # Base 01
+color11="e6/b6/73" # Base 13 - Bright Yellow
+color12="73/b8/ff" # Base 16 - Bright Blue
+color13="dd/bc/ff" # Base 17 - Bright Magenta
+color14="39/ba/e6" # Base 15 - Bright Cyan
+color15="f2/f0/e7" # Base 07 - Bright White
+color16="ff/8f/40" # Base 09
+color17="e6/b4/50" # Base 0F
+color18="13/17/21" # Base 01
 color19="20/22/29" # Base 02
-color20="89/8f/99" # Base 04
-color21="e6/e1/cf" # Base 06
-color_foreground="bf/bd/b6" # Base 05
-color_background="0f/13/1a" # Base 00
+color20="bf/bd/b6" # Base 04
+color21="ec/e8/db" # Base 06
+color_foreground="e6/e1/cf" # Base 05
+color_background="0b/0e/14" # Base 00
 
-if [ -n "$TMUX" ] || [ "${TERM%%[-.]*}" = "tmux" ]; then
+
+if [ -z "$TTY" ] && ! TTY=$(tty) || [ ! -w "$TTY" ]; then
+  put_template() { true; }
+  put_template_var() { true; }
+  put_template_custom() { true; }
+elif [ -n "$TMUX" ] || [ "${TERM%%[-.]*}" = "tmux" ]; then
   # Tell tmux to pass the escape sequences through
   # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
-  put_template() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' "$@"; }
-  put_template_var() { printf '\033Ptmux;\033\033]%d;rgb:%s\033\033\\\033\\' "$@"; }
-  put_template_custom() { printf '\033Ptmux;\033\033]%s%s\033\033\\\033\\' "$@"; }
+  put_template() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' "$@" > "$TTY"; }
+  put_template_var() { printf '\033Ptmux;\033\033]%d;rgb:%s\033\033\\\033\\' "$@" > "$TTY"; }
+  put_template_custom() { printf '\033Ptmux;\033\033]%s%s\033\033\\\033\\' "$@" > "$TTY"; }
 elif [ "${TERM%%[-.]*}" = "screen" ]; then
   # GNU screen (screen, screen-256color, screen-256color-bce)
-  put_template() { printf '\033P\033]4;%d;rgb:%s\007\033\\' "$@"; }
-  put_template_var() { printf '\033P\033]%d;rgb:%s\007\033\\' "$@"; }
-  put_template_custom() { printf '\033P\033]%s%s\007\033\\' "$@"; }
+  put_template() { printf '\033P\033]4;%d;rgb:%s\007\033\\' "$@" > "$TTY"; }
+  put_template_var() { printf '\033P\033]%d;rgb:%s\007\033\\' "$@" > "$TTY"; }
+  put_template_custom() { printf '\033P\033]%s%s\007\033\\' "$@" > "$TTY"; }
 elif [ "${TERM%%-*}" = "linux" ]; then
-  put_template() { [ "$1" -lt 16 ] && printf "\e]P%x%s" "$1" "$(echo "$2" | sed 's/\///g')"; }
+  put_template() { [ "$1" -lt 16 ] && printf "\e]P%x%s" "$1" "$(echo "$2" | sed 's/\///g')" > "$TTY"; }
   put_template_var() { true; }
   put_template_custom() { true; }
 else
-  put_template() { printf '\033]4;%d;rgb:%s\033\\' "$@"; }
-  put_template_var() { printf '\033]%d;rgb:%s\033\\' "$@"; }
-  put_template_custom() { printf '\033]%s%s\033\\' "$@"; }
+  put_template() { printf '\033]4;%d;rgb:%s\033\\' "$@" > "$TTY"; }
+  put_template_var() { printf '\033]%d;rgb:%s\033\\' "$@" > "$TTY"; }
+  put_template_custom() { printf '\033]%s%s\033\\' "$@" > "$TTY"; }
 fi
 
 # 16 color space
@@ -72,13 +77,13 @@ put_template 15 "$color15"
 # foreground / background / cursor color
 if [ -n "$ITERM_SESSION_ID" ]; then
   # iTerm2 proprietary escape codes
-  put_template_custom Pg bfbdb6 # foreground
-  put_template_custom Ph 0f131a # background
-  put_template_custom Pi bfbdb6 # bold color
+  put_template_custom Pg e6e1cf # foreground
+  put_template_custom Ph 0b0e14 # background
+  put_template_custom Pi e6e1cf # bold color
   put_template_custom Pj 202229 # selection color
-  put_template_custom Pk bfbdb6 # selected text color
-  put_template_custom Pl bfbdb6 # cursor
-  put_template_custom Pm 0f131a # cursor text
+  put_template_custom Pk e6e1cf # selected text color
+  put_template_custom Pl e6e1cf # cursor
+  put_template_custom Pm 0b0e14 # cursor text
 else
   put_template_var 10 "$color_foreground"
   if [ "$BASE24_SHELL_SET_BACKGROUND" != false ]; then
@@ -121,28 +126,28 @@ unset color_background
 
 # Optionally export variables
 if [ -n "$TINTED_SHELL_ENABLE_BASE24_VARS" ]; then
-  export BASE24_COLOR_00_HEX="0f131a"
-  export BASE24_COLOR_01_HEX="122132"
+  export BASE24_COLOR_00_HEX="0b0e14"
+  export BASE24_COLOR_01_HEX="131721"
   export BASE24_COLOR_02_HEX="202229"
-  export BASE24_COLOR_03_HEX="565b66"
-  export BASE24_COLOR_04_HEX="898f99"
-  export BASE24_COLOR_05_HEX="bfbdb6"
-  export BASE24_COLOR_06_HEX="e6e1cf"
-  export BASE24_COLOR_07_HEX="f3f4f5"
+  export BASE24_COLOR_03_HEX="3e4b59"
+  export BASE24_COLOR_04_HEX="bfbdb6"
+  export BASE24_COLOR_05_HEX="e6e1cf"
+  export BASE24_COLOR_06_HEX="ece8db"
+  export BASE24_COLOR_07_HEX="f2f0e7"
   export BASE24_COLOR_08_HEX="f07178"
-  export BASE24_COLOR_09_HEX="f29668"
+  export BASE24_COLOR_09_HEX="ff8f40"
   export BASE24_COLOR_0A_HEX="ffb454"
   export BASE24_COLOR_0B_HEX="aad94c"
   export BASE24_COLOR_0C_HEX="95e6cb"
   export BASE24_COLOR_0D_HEX="59c2ff"
-  export BASE24_COLOR_0E_HEX="6c5980"
-  export BASE24_COLOR_0F_HEX="e6b673"
-  export BASE24_COLOR_10_HEX="0d1017"
+  export BASE24_COLOR_0E_HEX="d2a6ff"
+  export BASE24_COLOR_0F_HEX="e6b450"
+  export BASE24_COLOR_10_HEX="0a0d13"
   export BASE24_COLOR_11_HEX="06070a"
-  export BASE24_COLOR_12_HEX="d95757"
-  export BASE24_COLOR_13_HEX="ff8f40"
+  export BASE24_COLOR_12_HEX="f26d78"
+  export BASE24_COLOR_13_HEX="e6b673"
   export BASE24_COLOR_14_HEX="7fd962"
-  export BASE24_COLOR_15_HEX="95e6cb"
-  export BASE24_COLOR_16_HEX="409fff"
-  export BASE24_COLOR_17_HEX="d2a6ff"
+  export BASE24_COLOR_15_HEX="39bae6"
+  export BASE24_COLOR_16_HEX="73b8ff"
+  export BASE24_COLOR_17_HEX="ddbcff"
 fi
