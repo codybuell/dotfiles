a8147d150d270e2d1ace2787ab0f7f92fdf31472
codybuell <cody@codybuell.com>
2026-07-20 08:53:31 -0400

    Change simulator screenshot location

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -628,6 +628,9 @@ defaults write com.apple.screencapture disable-shadow -bool "$DisableDrops"
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "$ScreenshotFormat"
 
+# set xcode simulator screenshots to same folder
+defaults write com.apple.iphonesimulator ScreenShotSaveLocation -string "$ScreenshotFolder"
+
 ################
 # TIME MACHINE #
 ################
